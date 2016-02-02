4eab242ade8fd9f6f1443ccaee75b013e438bc92
ruLegen <@>
2016-02-02 22:56:01 +0300

    Загрузил все файлы

---
+++ index.php
@@ -0,0 +1,217 @@
+
+<html>
+
+   <head>
+<meta name="viewport" content="width=device-width"> 
+<script type="text/javascript" src="js/jq.js"></script> 
+<script type="text/javascript" src="js/movie.js"> </script> 
+     <title>HumanWalk</title>
+    <style type="text/css">
+#floor{
+background:#130606;
+width:4320px;
+height:300px;
+position:absolute;
+left:0px;
+top:900px;
+z-index:20;
+}
+
+.back{
+background:navy;
+width:3000px;
+height:900px;
+position:fixed;
+left:0px;
+top:0px;
+z-index:-1;
+}
+.back1{
+background:url('img/bg.png');
+width:3000px;
+height:900px;
+position:absolute;
+left:0px;
+top:0px;
+z-index:1;
+}
+
+
+#h51{
+background:gray;
+width:200px;
+height:100px;
+position:absolute;
+top:200px;
+left:2200px;
+z-index:5;
+}
+#h52{
+background:black;
+width:300px;
+height:100px;
+position:absolute;
+top:200px;
+left:2400px;
+z-index:5;
+}
+#h53{
+background:gray;
+width:400px;
+height:100px;
+position:absolute;
+top:300px;
+left:2100px;
+z-index:5;
+}
+#h54{
+background:black;
+width:300px;
+height:100px;
+position:absolute;
+top:300px;
+left:2500px;
+z-index:5;
+}
+#h55{
+background:gray;
+width:600px;
+height:500px;
+position:absolute;
+top:400px;
+left:2000px;
+z-index:5;
+}
+#h56{
+background:black;
+width:300px;
+height:500px;
+position:absolute;
+top:400px;
+left:2600px;
+z-index:5;
+}
+
+#P2part{
+background:black;
+width:200px;
+height:300px;
+position:absolute;
+top:600px;
+left:2200px;
+z-index:6;
+}
+.human{
+background:url('img/1.png');
+width:170px;
+height:300px;
+position:absolute;
+top:608px;
+left:0px;
+z-index:6;
+}
+#X1{
+background:dimgray;
+width:80px;
+height:400px;
+position:absolute;
+top:500px;
+left:1400px;
+z-index:4;
+outline: 20px solid black;
+}
+#X2{
+background:dimgray;
+width:300px;
+height:80px;
+position:absolute;
+top:600px;
+left:1290px;
+z-index:3;
+outline: 20px solid black;
+}
+#X3{
+background:url('img/14.bmp');
+width:150px;
+height:100px;
+position:absolute;
+top:590px;
+left:1365px;
+z-index:5;
+outline: 2px solid black;
+}
+#x3:hover {
+-webkit-transform: scale(3);
+-moz-transform: scale(3);
+-o-transform: scale(3);
+}
+#moon{
+background:url('img/moon.png');
+width:200px;
+height:200px;
+position:fixed;
+top:px;
+left:100px;
+z-index:0;
+}
+#panell{
+width:50px;
+height:50px;
+position:fixed;
+top:10px;
+left:10px;
+z-index:5;
+outline: 5px solid black;
+
+}
+#panell2{
+width:50px;
+height:50px;
+position:fixed;
+top:10px;
+left:70px;
+z-index:5;
+outline: 5px solid black;
+
+}
+
+
+</style>
+   </head>
+
+   <body>
+
+     <div class="back"></div>
+     <section>
+     <div class="back1"></div>
+     </section>
+     <div id="floor"></div>
+     <div id="f31"></div>
+     <div id="f32"></div>
+     <div id="f33"></div>
+     <div id="f331"></div>
+     <div id="f34"></div>
+     <div id="f35"></div>
+
+     <div id="h51"></div>
+     <div id="h52"></div>
+     <div id="h53"></div>
+
+     <div id="h54"></div>
+     <div id="h55"></div>
+     <div id="h56"></div>
+
+     <div id="P2part"></div>
+     <div class="human"></div>
+     <div id="X1"></div>
+     <div id="X2"></div> 
+     <div id="X3"></div>
+     <div id="moon"></div>
+     <div id="panell"></div>
+     <div id="panell2"></div>  
+
+   </body>
+
+
+ </html>
+
---
+++ js/movie.js
@@ -0,0 +1,178 @@
+var stop = false; 
+var _left=0 ;  
+var _top; 
+var _height; 
+var step = 9; 
+var borderCam = 350; 
+var camOffset = 0; 
+var camOffsetRight = 0; 
+var dir; // направление для анимации 
+var name = ""; 
+var divsLng = 0; // колличество DIV на стр 
+var ids = []; // idDivoв 
+var divs; // все DIVы 
+var shift = false; 
+var isOnPortal = false; 
+$(window).resize(function(){ 
+
+  //window.scrollTo(window.pageXOffset,document.documentElement.clientHeight/3); 
+   // alert(""); 
+}); 
+
+function checkDivs() 
+{ 
+  divsLng =  $('div').length; 
+  divs = $('div'); 
+
+for(var i = 0; i < divsLng; i++) 
+  { 
+   var did = divs[i].id; 
+   if(did[0] == 'P') 
+   { 
+    ids.push(did); 
+   } 
+  // alert("ok " + i +"  "+ did + "  " + $('div[id='+did+']').position().left); 
+  } 
+
+} 
+
+function goToPortal() 
+{ 
+  var humanPosition = parseInt($('.human').position().left) + parseInt($('.human').css("width"))/2; //положение человека + половина ширины 
+   for(var i = 0; i < ids.length; i++) 
+   { 
+
+var portalPos = $('div[id='+ids[i]+']').position().left //край положения портала 
+    var portalWidth = parseInt( $('div[id='+ids[i]+']').css("width")); //ширина портала 
+    if(humanPosition > portalPos && humanPosition < portalPos + portalWidth) 
+    { 
+     window.location.href = ids[i] + '.html'; 
+     return true; 
+    } 
+    else 
+     return false; 
+   } 
+} 
+
+
+function portalCheck() 
+{ 
+  var humanPosition = parseInt($('.human').position().left) + parseInt($('.human').css("width"))/2; //положение человека + половина ширины 
+   for(var i = 0; i < ids.length; i++) 
+   { 
+
+var portalPos = $('div[id='+ids[i]+']').position().left //край положения портала 
+    var portalWidth = parseInt( $('div[id='+ids[i]+']').css("width")); //ширина портала 
+    if(humanPosition > portalPos && humanPosition < portalPos + portalWidth) 
+     return true; 
+    else 
+     return false; 
+   } 
+} 
+
+$(document).ready(function() { 
+   
+ document.body.style.overflow = "hidden"; 
+    window.scrollTo(window.pageXOffset,document.documentElement.clientHeight/3,5); 
+
+    checkDivs(); 
+ // для шифта.. Бег 
+  $(document).keyup(function(e){ 
+  var key = e.keyCode; 
+
+switch(key) 
+  { 
+   case 16: shift = !shift;break; 
+  } 
+  if(shift) 
+  {  
+   step = 20;  
+   $('#panell').css('background-image', "url('img/run.bmp')");  
+  }  
+  else 
+  {  
+   step = 9;  
+   $('#panell').css('background-image', "url('')");  
+  }  
+
+}); 
+     
+  
+  $(document).keydown(function(e){ 
+ var key = e.keyCode; 
+  if(!stop) 
+  { 
+   switch(key) 
+   { 
+    case 68: _left+=step; dir = 1;break; 
+    case 65: 
+    if($('.human').position().left > 0) 
+    {_left-= step;} 
+    dir = 0; 
+    $('.human').css('background-image', "url('img/walk_revers.gif')");break; 
+
+} 
+
+if(key == 68 || key == 65) 
+   switch(dir) 
+   { 
+    case 1:$('.human').css('background-image', "url('img/walk.gif')");break; 
+    case 0:$('.human').css('background-image', "url('img/walk_revers.gif')");break 
+   } 
+
+var _clientWidth = document.documentElement.clientWidth; //Ўирина клиентской частм 
+
+//$().scrollTo(document); 
+   if($('.human').position().left >(_clientWidth + window.pageXOffset - borderCam)) 
+   { 
+
+window.scrollBy(step,0); 
+   } 
+
+if($('.human').position().left <(window.pageXOffset + borderCam)) 
+   { 
+    window.scrollBy(-step,0); 
+   } 
+
+$('.human').offset({left:_left}); 
+  } 
+  isOnPortal = portalCheck(); 
+
+if(isOnPortal) 
+  { 
+   $('#panell2').css('background-image',"url('img/dor.bmp')") 
+  } 
+  else 
+  { 
+   $('#panell2').css('background-image',"url('*')") 
+  } 
+ // переход на др локацию 
+  if(key == 69) 
+  { 
+   goToPortal(); 
+  } 
+
+}); 
+
+$(document).keyup(function(e){ 
+ stop = false; 
+ $('.human').css('background-image', "url('img/1.png')"); 
+ }); 
+
+}); 
+
+
+function parallax(){ 
+    var scrolled = $(window).scrollLeft(); 
+$('.back1').css('left', +(scrolled) + 'px'); 
+ } 
+
+function parallax(){ 
+    var scrolled = $(window).scrollLeft(); 
+    $('.back1').css('left', +(scrolled * 0.5) + 'px'); 
+ } 
+$(window).scroll(function(e){ 
+    parallax(); 
+ window.scrollTo(window.pageXOffset,document.documentElement.clientHeight/3,5); 
+
+});
