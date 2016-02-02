4fcfccb1359e6b940c6201ec5dd3aa66d6923c00
HAHDHDS <sfahdhghadg@mail.ru>
2016-02-02 23:54:09 +0300

    ЭЭЭЭ
    Ну я .эээ. добавид третью сецкию

--- js/movie.js
+++ js/movie.js
@@ -13,7 +13,6 @@ var ids = []; // idDivoв
 var divs; // все DIVы 
 var shift = false; 
 var isOnPortal = false; 
-//измни ыввы
 $(window).resize(function(){ 
 
   //window.scrollTo(window.pageXOffset,document.documentElement.clientHeight/3); 
