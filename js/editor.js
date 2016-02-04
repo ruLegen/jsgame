ae577cd3eace35d2eed055b91b0da89912fb6ee2
ruLegen <@>
2016-02-04 19:05:32 +0300

    Рабочая версия блоков
    Добавил Непрозрачность и Слой для блоков

--- js/editor.js
+++ js/editor.js
@@ -1,13 +1,62 @@
 ﻿var state;
 var selected;
+var blockCount = 0;
 var _width;
 var _height;
+var _color;
+var _file;
+var _layer;
+var _opacity = 0;
 var _x;
 var _y;
+function createDiv(e)
+{
+	_x = e.clientX;
+			_y = e.clientY;
+				
+			if(state == 'block')
+			{
+				$('div[id=editorField]').append("<div id=block" + blockCount + "></div>");
+				var obj = $("div[id=block"+blockCount+']');
+				obj.css({
+					"width" : _width,
+					"height" : _height,
+					"left": _x,
+					"top":_y,
+					"position":"absolute",
+					"backgroundColor": _color,
+					"opacity": _opacity,
+					"z-index": _layer
+					});
+			}
+			
+			if(state == 'picture')
+			{
+				
+			}
+			blockCount++;
+}
+function cursorEveryFrame(e)
+{
+			_x = e.clientX;
+			_y = e.clientY;
+				
+			if(state == 'block')
+			{
+				
+				var obj = $("div[id=cursor]");
+				obj.css({"width" : _width,"height" : _height,"left": _x,"top":_y,"position":"absolute","backgroundColor": _color,"background-blend-mode":"overlay","opacity":"0.5"});
+			}
+			
+			if(state == 'picture')
+			{
+				
+			}
+}
 
 $(document).ready(function(){
 	
-	$('input[name=blck]').click(function(){
+	$('input[name=blck]').change(function(){
 	
 		var r =$('input[name=blck]');
 		
@@ -21,8 +70,9 @@ $(document).ready(function(){
 			$('div[id=choose]').html(
 			"Ширина <input id='_width' size='6' type='textbox'/></h1></br></br>"+
 			"Высота <input id='_height' size='6' type='textbox'/></h1></br></br>"+
-			"Цвет <input id='_color' type='color'/>"+
-			""
+			"Цвет <input id='_color' type='color'/></br></br>"+
+			"Непрозрачность <input id='opacity' type='number' min = '0' max = '100'/></br></br>"+
+			"Слой <input id='index' type='number'/>"
 			);
 		}
 		if(state == 'picture')
@@ -30,16 +80,47 @@ $(document).ready(function(){
 			$('div[id=choose]').html(
 			"Ширина <input id='_width' size='6' type='textbox'/></h1></br></br>"+
 			"Высота <input id='_height' size='6' type='textbox'/></h1></br></br>"+
-			"Картинка <input id='_picture' type='file'/>"+
-			""
+			"Картинка <input id='_picture' accept='image/*' type='file'/></br></br>"+
+			"Непрозрачность <input id='opacity' type='number' min = '0' max = '100'/></br></br>"+
+			"Слой <input id='index' type='number'/>"
 			);
 		}
 		
 	});
+	
 		
 		$('#ok').click(function(){
 			
-			alert($('#_picture').val());
-		
+			_width = parseInt($('#_width').val());
+			_height = parseInt($('#_height').val());
+			_color = $('#_color').val();
+			try{_file = document.getElementById("_picture").files['0'].name;}
+			catch(e){}
+			
+			_opacity = parseInt($('input[id=opacity]').val());
+				if(isNaN(_opacity))
+					_opacity = 100;
+				if(_opacity > 100)
+					_opacity = 100;
+			_opacity = _opacity/100;
+			
+			_layer = parseInt($('input[id=index]').val());
+			if(isNaN(_layer))
+					_layer = 0;
+			alert(_width + ' ' +_height + ' ' + _color + ' '+ _file);
 		});
+		
+		$('div[id=cursor]').click(function(e){
+			createDiv(e);
+		});
+		
+		$('div[id=editorField]').mousemove(function(e){
+			cursorEveryFrame(e);
+		});
+		
+		$('div[id=cursor').mousemove(function(e){
+			cursorEveryFrame(e);
+			
+		});
+		
 });
