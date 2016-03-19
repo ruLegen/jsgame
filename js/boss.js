28b7af84efc9f3d23ed715932d414847342f1631
ruLegen <forgame98@yandex.ru>
2016-03-19 20:57:55 +0400

    ДОбавлен минимальный босс

--- js/boss.js
+++ js/boss.js
@@ -1,52 +1,82 @@
+var human =""
+var bossStart = 3300;
+var whenBossSpawn = 3000;
+var boss = "";
+var i = 0;
+var allowShot = true;
+function onLoadDocument()
+{
+	var human = $('.human');
+}
 function Boss ()
 {
-	this.stepLength = 4;
-	this.boss = $(boss);
-	this.isDead = false;
-	this.hittedCount = 0;
-	this.maxHits = 50;
-	this.x = 0;
-	this.y = $('.human').position().top;
-	this.walkSpeed = 5;
-	this.img = "";
-	this.dir = 1;
-	this.imgDir = "img/";
+	
 	this.width = 500;
 	this.height = 500;
+	this.stepLength = 4;
+	this.boss = $('boss');
+	this.isDead = false;
+	this.hittedCount = 0;
+	this.maxHits = 3;
+	this.x = bossStart;
+	this.y = parseInt($('.human').position().top -(this.height-$('.human').height()));
+	this.walkSpeed = 5;
+	this.damagePersent = this.width / (this.maxHits + 1);
+	this.dir = 1;
 	
-	this.startFunction = function(){
-		this.boss.css({
-			"position":"absolution",
-			"width": this.width,
-			"height": this.height,
-			"left":this.x,
-			"top":this.y,
-			"background":"url("+this.imgDir+this.img+")"
-			
-		});
-	}
+	this.img = "huileft.gif";
+	this.imgDir = "img/";
+	this.fullDir = this.imgDir+this.img;
+	
+	this.deathImg = "death.gif?" + Math.random(0,999);
+	this.deathFullDir = this.imgDir+this.deathImg;
 	
 	this.updatePos = function(){
 		this.boss.css("left",this.x);
+		this.boss.css("top",this.y);
+		
 	}
-	this.setImg= function setImage(img){
-		this.img = this.imgDir + img;
-		this.boss.css({"background":"url("+this.img+")"});			//“ут будет блок босса
+
+	
+	this.setPos = function(x)
+	{
+		this.x = x;
+		this.updatePos();
+		//this.y = y;   //пока не надо
+	}
+	
+	
+	
+		this.setImg= function setImage(img){
+		this.boss.css({"background":"url("+img+")"});			//“ут будет блок босса
 	}
 	this.kill = function(){
 		this.isDead = true;
-		this.setImg('');									//“”т люба€ картинка при смерти
+		this.setImg(this.deathFullDir);									//“”т люба€ картинка при смерти
 	}
 	
-	this.hitted = function (){
-		if(this.hittedCount < this.maxHits)
-		{
-			this.hittedCount++;
-		}
-		else
-		{
-			this.kill();
-		}
+	this.hitted = function (){														///при попадании
+		var tempBoss = this;
+		var timeID = setTimeout(function(){
+			if(!tempBoss.isDead)
+			{
+				$("#green").animate(
+				{
+					width : $("#green").width() - tempBoss.damagePersent
+				},100);
+			}
+			if(tempBoss.hittedCount < tempBoss.maxHits)
+			{
+				tempBoss.hittedCount++;
+			}
+			else
+			{
+				tempBoss.kill();
+				$("#health").hide(500);
+			}
+			
+			clearTimeout(timeID);
+			},400);
 	}
 	
 	this.moveBy = function(){
@@ -61,7 +91,72 @@ function Boss ()
 		}
 		this.updatePos();
 	}
+	this.startFunction = function ff(){
 	
+		if($('boss').length == 0)
+		{
+			$('body').append('<boss></boss>');
+			$('boss').append("<div id ='health'></div>");
+			$('#health').append("<div id ='red'></div>");
+			$('#health').append("<div id ='green'></div>");
+			$('#health').css({
+				"position" : "relative",
+				"width" : this.width,
+				"height" : "20px",
+				"border-radius" : "4px"
+			});
+			$('#red').css({
+				"position":"fixed",
+				"width" : this.width,
+				"height" : "20px",
+				"border-radius" : "4px",
+				"background" : "#DE0D0D"
+			});
+			$('#green').css({
+				"position":"fixed",
+				"width" : this.width,
+				"height" : "20px",
+				"border-radius" : "4px",
+				"background" : "#19CC49"
+			});
+			this.boss = $('boss');
+		}
+		else
+			alert("пожалуйста удалите из html документа все теги <boss></boss>");
+		
+		this.boss.css({
+			"position":"absolute",
+			"width": this.width,
+			"height": this.height,
+			"left":this.x,
+			"top":this.y,
+			"background":"url("+this.fullDir+")"
+		});
+	}
 	this.startFunction();
+	
+	var tempBoss = this;  	// для того что бы можно было делать
+	
+	$('boss').click(function(){
+		//var timeID = setTimeout(function(){allowShot = true;},500);
+	
+				if(mouse == 1 && currentAmmo > 0 && !humanDead)							///////////застрял здесь
+					{
+						allowShot = false;
+						tempBoss.hitted();
+						//clearTimeout(timeID);
+					}
+			});
+	
 }
 
+$(document).ready(function(){
+	onLoadDocument();
+	var intervalID = setInterval(function(){
+		if($('.human').position().left > whenBossSpawn )// && (maxEnemy - countKilledEnemy) <= 0) //shot
+		{
+			boss = new Boss();
+			clearInterval(intervalID);
+		}
+	},2);
+});
